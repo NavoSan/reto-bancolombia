5092aa2139e080131d01973e08a71f6c78c2a5f2
Santiago Navales Parra <santiagonavalesp@gmail.com>
2024-04-18 20:56:39 -0500

    Se anade flask-socketio al consumidor, para que la lectura se haga con un enfoque por websockets en tiempo real

--- consumer/app.py
+++ consumer/app.py
@@ -2,40 +2,38 @@ from flask import Flask, jsonify, render_template
 from rutas import main
 from kafka import KafkaConsumer
 import json
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
 app.register_blueprint(main)
+socketio = SocketIO(app)
 
-consumer = KafkaConsumer(
-    'github-events',
-    'azure-events',
-    'gitlab-events',
-    bootstrap_servers='kafka:29092',
-    auto_offset_reset='earliest',
-    group_id='my-group-id',
-    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-)
+def kafka_consumer():
+    consumer = KafkaConsumer(
+        'github-events',
+        'azure-events',
+        'gitlab-events',
+        bootstrap_servers='kafka:29092',
+        auto_offset_reset='latest',
+        group_id='stream-consumer',
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    )
+    for message in consumer:
+        socketio.emit('new_message', {'data': message.value}, namespace='/stream')
+
 
 @app.route('/consume', methods=['GET'])
 def consume():
-    messages = []
-    try:
-        # Consume messages
-        for message in consumer:
-            messages.append({
-                'topic': message.topic,
-                'message': message.value
-            })
-            # Solo para demostración, podrías querer tener un control más sofisticado
-            if len(messages) >= 10:  # Limita a 10 mensajes para no atascar el endpoint
-                break
-        return render_template('consume.html', messages=messages)
-    
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    return render_template('consume.html', messages=messages)
 
-# Registro del Blueprint del archivo rutas.py
+@socketio.on('connect', namespace='/stream')
+def test_connect():
+    app.logger.info("Client connected")
 
+@socketio.on('disconnect', namespace='/stream')
+def test_disconnect():
+    app.logger.info("Client disconnected")
 
 
 if __name__ == '__main__':
